4bad21b0f67b4f4e7acaeac638d4e831ca79c676
psalvi <salvipablo@outlook.com>
2023-07-07 15:45:25 -0300

    agregado tipo de vehiculo
    Ahora la concesionaria vende motos
    tambien con otro impuestos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,35 +3,56 @@ package com.company;
 import java.util.ArrayList;
 
 public class Main {
-    public static ArrayList<Car> cars = new ArrayList<>();
+    public static ArrayList<Vehicles> cars = new ArrayList<>();
 
     public static void main(String[] args) {
-        Car car_1 = new Car("Gol", "Trend 1.6 Pack3", 2010, true, 25000);
-        Car car_2 = new Car("Vento", "2.5 Deluxe", 2008, true, 45000);
-        Car car_3 = new Car("Chevrolet", "Corsa 1.3", 2005, false, 12000);
-        Car car_4 = new Car("Renault", "Megane 2.0 Coupe", 2003, true, 13000);
+        Vehicles veh_1 = new Vehicles("Gol", "Trend 1.6 Pack3", 2010, true, 25000, "auto");
+        Vehicles veh_2 = new Vehicles("Vento", "2.5 Deluxe", 2008, true, 45000, "auto");
+        Vehicles veh_3 = new Vehicles("Chevrolet", "Corsa 1.3", 2005, false, 12000, "auto");
+        Vehicles veh_4 = new Vehicles("Renault", "Megane 2.0 Coupe", 2003, true, 13000, "auto");
 
-        cars.add(car_1);
-        cars.add(car_2);
-        cars.add(car_3);
-        cars.add(car_4);
+        Vehicles veh_5 = new Vehicles("Honda", "Wawe 110", 2002, false, 6000, "moto");
+        Vehicles veh_6 = new Vehicles("Zanella", "110 TTE", 2008, false, 7500, "moto");
+
+        cars.add(veh_1);
+        cars.add(veh_2);
+        cars.add(veh_3);
+        cars.add(veh_4);
+        cars.add(veh_5);
+        cars.add(veh_6);
 
         listarTodosPrecioImpuesto();
     }
 
     public static void listarTodosPrecioImpuesto() {
-        for (Car car: cars) {
-            System.out.println(car.getBrand() + "-" + car.getDescription() + "-" + (car.getPrice() * 1.15));
+        for (Vehicles car: cars) {
+            if (car.getTypeVehicle().equals("auto")) {
+                System.out.println(car.getBrand() + "-" + car.getDescription() + "-" + (car.getPrice() * 1.20) +
+                                                                                                " - 20% de impuesto");
+            } else {
+                System.out.println(car.getBrand() + "-" + car.getDescription() + "-" + (car.getPrice() * 1.10) +
+                                                                                                " - 10% de impuesto");
+            }
+
         }
     }
 }
 
-class Car {
+class Vehicles {
     private String brand;
     private String description;
     private int model;
     private boolean airConditioning;
     private int price;
+    private String typeVehicle;
+
+    public String getTypeVehicle() {
+        return typeVehicle;
+    }
+
+    public void setTypeVehicle(String typeVehicle) {
+        this.typeVehicle = typeVehicle;
+    }
 
     public int getPrice() {
         return price;
@@ -73,11 +94,13 @@ class Car {
         this.airConditioning = airConditioning;
     }
 
-    public Car(String brand, String description, int model, boolean airConditioning, int price) {
+    public Vehicles(String brand, String description, int model, boolean airConditioning, int price,
+                                                                                                String typeVehicle) {
         this.brand = brand;
         this.description = description;
         this.model = model;
         this.airConditioning = airConditioning;
         this.price = price;
+        this.typeVehicle = typeVehicle;
     }
 }
