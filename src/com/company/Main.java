416837988a19721be04a93e4e3789fd8e8902927
psalvi <salvipablo@outlook.com>
2023-07-07 16:36:21 -0300

    Refactor
    Se agrego clase que implementa
    singleton y lleva el control de los ids

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,31 +7,23 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         VehiclesPersistence vehiclesPersistence = new VehiclesPersistence();
-        int id = 0;
+        VehiclesIds idsVehiclesInf = VehiclesIds.getInstance();
 
-        id++;
+        // Autos.
         Vehicles veh_1 = new Vehicles("Gol", "Trend 1.6 Pack3", 2010, true, 25000, "auto");
-        vehiclesPersistence.createVehicle(id, veh_1);
-
-        id++;
         Vehicles veh_2 = new Vehicles("Vento", "2.5 Deluxe", 2008, true, 45000, "auto");
-        vehiclesPersistence.createVehicle(id, veh_2);
-
-        id++;
         Vehicles veh_3 = new Vehicles("Chevrolet", "Corsa 1.3", 2005, false, 12000, "auto");
-        vehiclesPersistence.createVehicle(id, veh_3);
-
-        id++;
         Vehicles veh_4 = new Vehicles("Renault", "Megane 2.0 Coupe", 2003, true, 13000, "auto");
-        vehiclesPersistence.createVehicle(id, veh_4);
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_1);
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_2);
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_3);
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_4);
 
-        id++;
+        // Motos
         Vehicles veh_5 = new Vehicles("Honda", "Wawe 110", 2002, false, 6000, "moto");
-        vehiclesPersistence.createVehicle(id, veh_5);
-
-        id++;
         Vehicles veh_6 = new Vehicles("Zanella", "110 TTE", 2008, false, 7500, "moto");
-        vehiclesPersistence.createVehicle(id, veh_6);
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_5);
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_6);
 
         listarTodosPrecioImpuesto(vehiclesPersistence.getCars());
 
@@ -47,10 +39,10 @@ public class Main {
     public static void listarTodosPrecioImpuesto(Map<Integer, Vehicles> cars) {
         for (Map.Entry<Integer, Vehicles> car: cars.entrySet()) {
             if (car.getValue().getTypeVehicle().equals("auto")) {
-                System.out.println(car.getValue().getBrand() + " - " + car.getValue().getDescription() + "-" +
+                System.out.println(car.getKey() + " - " + car.getValue().getBrand() + " - " + car.getValue().getDescription() + "-" +
                                                             (car.getValue().getPrice() * 1.20) + " - 20% de impuesto");
             } else {
-                System.out.println(car.getValue().getBrand() + " - " + car.getValue().getDescription() + "-" +
+                System.out.println(car.getKey() + " - " + car.getValue().getBrand() + " - " + car.getValue().getDescription() + "-" +
                                                             (car.getValue().getPrice() * 1.10) + " - 10% de impuesto");
             }
         }
---
+++ src/com/company/VehiclesIds.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class VehiclesIds {
+    private static VehiclesIds vehiclesIds;
+    private final ArrayList<Integer> ids = new ArrayList<>();
+
+    private VehiclesIds() {}
+
+    public static VehiclesIds getInstance() {
+        if (vehiclesIds == null) vehiclesIds = new VehiclesIds();
+        return vehiclesIds;
+    }
+
+    public int nextId() {
+        ids.add(this.ids.size() + 1);
+        return this.ids.size();
+    }
+}
