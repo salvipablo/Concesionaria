ec51a4ef42f8d7a25e9490d0a2238cf77eb1485b
psalvi <salvipablo@outlook.com>
2023-07-07 15:54:27 -0300

    Agregado de ID y visualizacion individual

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,21 @@ public class Main {
     public static ArrayList<Vehicles> cars = new ArrayList<>();
 
     public static void main(String[] args) {
-        Vehicles veh_1 = new Vehicles("Gol", "Trend 1.6 Pack3", 2010, true, 25000, "auto");
-        Vehicles veh_2 = new Vehicles("Vento", "2.5 Deluxe", 2008, true, 45000, "auto");
-        Vehicles veh_3 = new Vehicles("Chevrolet", "Corsa 1.3", 2005, false, 12000, "auto");
-        Vehicles veh_4 = new Vehicles("Renault", "Megane 2.0 Coupe", 2003, true, 13000, "auto");
+        int id = 0;
 
-        Vehicles veh_5 = new Vehicles("Honda", "Wawe 110", 2002, false, 6000, "moto");
-        Vehicles veh_6 = new Vehicles("Zanella", "110 TTE", 2008, false, 7500, "moto");
+        id++;
+        Vehicles veh_1 = new Vehicles(id, "Gol", "Trend 1.6 Pack3", 2010, true, 25000, "auto");
+        id++;
+        Vehicles veh_2 = new Vehicles(id, "Vento", "2.5 Deluxe", 2008, true, 45000, "auto");
+        id++;
+        Vehicles veh_3 = new Vehicles(id, "Chevrolet", "Corsa 1.3", 2005, false, 12000, "auto");
+        id++;
+        Vehicles veh_4 = new Vehicles(id, "Renault", "Megane 2.0 Coupe", 2003, true, 13000, "auto");
+
+        id++;
+        Vehicles veh_5 = new Vehicles(id, "Honda", "Wawe 110", 2002, false, 6000, "moto");
+        id++;
+        Vehicles veh_6 = new Vehicles(id, "Zanella", "110 TTE", 2008, false, 7500, "moto");
 
         cars.add(veh_1);
         cars.add(veh_2);
@@ -22,6 +30,14 @@ public class Main {
         cars.add(veh_6);
 
         listarTodosPrecioImpuesto();
+
+        System.out.println();
+        System.out.println("---------------------------");
+        System.out.println();
+
+        showVehicle(1);
+        showVehicle(3);
+        showVehicle(5);
     }
 
     public static void listarTodosPrecioImpuesto() {
@@ -36,15 +52,29 @@ public class Main {
 
         }
     }
+
+    public static void showVehicle(int id) {
+        for (Vehicles vehicle: cars) {
+            if (vehicle.getId() == id) {
+                System.out.println(vehicle.getBrand() + "-" + vehicle.getDescription() + "-" +
+                                                                (vehicle.getPrice() * 1.20) + " - 20% de impuesto");
+            }
+        }
+    }
 }
 
 class Vehicles {
+    private int id;
     private String brand;
     private String description;
     private int model;
     private boolean airConditioning;
     private int price;
     private String typeVehicle;
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
 
     public String getTypeVehicle() {
         return typeVehicle;
@@ -94,8 +124,9 @@ class Vehicles {
         this.airConditioning = airConditioning;
     }
 
-    public Vehicles(String brand, String description, int model, boolean airConditioning, int price,
+    public Vehicles(int id, String brand, String description, int model, boolean airConditioning, int price,
                                                                                                 String typeVehicle) {
+        this.id = id;
         this.brand = brand;
         this.description = description;
         this.model = model;
