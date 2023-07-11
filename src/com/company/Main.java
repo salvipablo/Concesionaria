9437773d50bd141886583e134baa049bed6c403a
psalvi <salvipablo@outlook.com>
2023-07-11 08:53:06 -0300

    Refactor
    Reemplazo de las lineas veh_1, veh_2
    para un ordenamiento y sacar lineas de
    programacion demas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,22 +9,18 @@ public class Main {
         VehiclesPersistence vehiclesPersistence = new VehiclesPersistence();
         VehiclesIds idsVehiclesInf = VehiclesIds.getInstance();
 
-        // TODO: Sacar veh_1, veh_2 etc, hacer que se pueda crear mas dinamicamente y despues con un for enviar a persistencia
-        // Autos.
-        Vehicles veh_1 = new Car("Gol", "Trend 1.6 Pack3", 2010, true, 25000, "auto");
-        Vehicles veh_2 = new Car("Vento", "2.5 Deluxe", 2008, true, 45000, "auto");
-        Vehicles veh_3 = new Car("Chevrolet", "Corsa 1.3", 2005, false, 12000, "auto");
-        Vehicles veh_4 = new Car("Renault", "Megane 2.0 Coupe", 2003, true, 13000, "auto");
-        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_1);
-        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_2);
-        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_3);
-        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_4);
-
-        // Motos
-        Vehicles veh_5 = new Motorcycle("Honda", "Wawe 110", 2002, false, 6000, "moto");
-        Vehicles veh_6 = new Motorcycle("Zanella", "110 TTE", 2008, false, 7500, "moto");
-        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_5);
-        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(), veh_6);
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(),
+                                                        new Car("Gol", "Trend 1.6 Pack3", 2010, true, 25000, "auto"));
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(),
+                                                            new Car("Vento", "2.5 Deluxe", 2008, true, 45000, "auto"));
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(),
+                                                        new Car("Chevrolet", "Corsa 1.3", 2005, false, 12000, "auto"));
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(),
+                                                    new Car("Renault", "Megane 2.0 Coupe", 2003, true, 13000, "auto"));
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(),
+                                                    new Motorcycle("Honda", "Wawe 110", 2002, false, 6000, "moto"));
+        vehiclesPersistence.createVehicle(idsVehiclesInf.nextId(),
+                                                    new Motorcycle("Zanella", "110 TTE", 2008, false, 7500, "moto"));
 
         // TODO: Hacer que la aplicacion este mas ordenada con una clase Controller y la ejecucion salga de main
 
--- src/com/company/Vehicles.java
+++ src/com/company/Vehicles.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 abstract public class Vehicles {
     private String brand;
     private String description;
